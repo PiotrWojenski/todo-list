ddc868c25c95b9d4ab34ad65240658852cbb56a4
Piotr Wojenski <pwojenskip@gmail.com>
2024-01-01 21:30:01 +0100

    Refactor App and AddTask components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
-import React, { useState } from 'react'
 import './App.css'
 import AddTask from './components/addTasks/AddTask'
 
 function App() {
-	const [tasks, setTasks] = useState<any>([])
-
-	const taskHandler = (task: any) => {
-		setTasks([...tasks, task])
-	}
-	console.log(tasks)
-
 	return (
-		<div>
-			<AddTask addTask={taskHandler} />
+		<div className="App">
+			<AddTask />
 		</div>
 	)
 }
--- src/components/addTasks/AddTask.tsx
+++ src/components/addTasks/AddTask.tsx
@@ -1,30 +1,25 @@
 import React, { useState } from 'react'
 
-const AddTask = (props: any) => {
+const AddTask = () => {
 	const [inputValue, setInputValue] = useState('')
-	const { v4: uuidv4 } = require('uuid')
 
 	const addText = (e: any) => {
 		setInputValue(e.target.value)
 	}
-	const addNewTask = () => {
-		const newTask = {
-			id: uuidv4(),
-			value: inputValue,
-			isCompleted: false,
-		}
-		console.log(newTask)
-		props.addTask(newTask)
-	}
-
-	console.log(inputValue)
 
 	return (
-		<div className="flex">
-			<input value={inputValue} onChange={addText} type="text" />
-			<button className="bg-sky-500 rounded " onClick={addNewTask}>
-				Add task
-			</button>
+		<div className="d-flex justify-center">
+			<div>
+				<input
+					className=" flex-1 p-2 m-2 border-2 border-solid border-blue-800 rounded-xl"
+					value={inputValue}
+					onChange={addText}
+					type="text"
+				/>
+			</div>
+			<div>
+				<button className=" flex-1 p-2 m-2 border-solid border-2  border-blue-800 rounded-md">Add Task</button>
+			</div>
 		</div>
 	)
 }
