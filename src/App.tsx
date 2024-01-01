a957034918c8cf6cb53b91a6966936c4782fb969
Piotr Wojenski <pwojenskip@gmail.com>
2024-01-01 21:38:16 +0100

    Add task handler and update AddTask component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css'
 import AddTask from './components/addTasks/AddTask'
+import { useState } from 'react'
 
 function App() {
+	const [tasksList, setTasksList] = useState<any>([])
+
+	const taskHandler = (task: any) => {
+		setTasksList([...tasksList, task])
+	}
+
 	return (
 		<div className="App">
-			<AddTask />
+			<AddTask addNewTask={taskHandler} />
 		</div>
 	)
 }
--- src/components/addTasks/AddTask.tsx
+++ src/components/addTasks/AddTask.tsx
@@ -1,14 +1,21 @@
 import React, { useState } from 'react'
 
-const AddTask = () => {
+const AddTask = (props: any) => {
 	const [inputValue, setInputValue] = useState('')
+	const { v4: uuidv4 } = require('uuid')
 
 	const addText = (e: any) => {
 		setInputValue(e.target.value)
 	}
 
 	const addTask = () => {
-		console.log(inputValue)
+		const task = {
+			id: uuidv4(),
+			value: inputValue,
+			isCompleted: false,
+		}
+		console.log(task)
+		props.taskHandler(task)
 	}
 
 	return (
