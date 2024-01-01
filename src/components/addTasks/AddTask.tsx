2922c42e912a797bda41967e8c57688368ce163c
Piotr Wojenski <pwojenskip@gmail.com>
2024-01-01 21:52:54 +0100

    Refactor AddTask component and update index.css

--- src/components/addTasks/AddTask.tsx
+++ src/components/addTasks/AddTask.tsx
@@ -15,21 +15,21 @@ const AddTask = (props: any) => {
 			isCompleted: false,
 		}
 		console.log(task)
-		props.taskHandler(task)
+		props.addNewTask(task)
 	}
 
 	return (
-		<div className="d-flex justify-center">
-			<div>
+		<div className="flex flex-row  h-dvh bg-blue-400  ">
+			<div className="flex-2">
 				<input
-					className=" flex-1 p-2 m-2 border-2 border-solid border-blue-800 rounded-xl"
+					className="  p-2 m-2 border-2 border-solid border-blue-800 rounded-xl"
 					value={inputValue}
 					onChange={addText}
 					type="text"
 				/>
 			</div>
-			<div>
-				<button className=" flex-1 p-2 m-2 border-solid border-2  border-blue-800 rounded-md" onClick={addTask}>
+			<div className="flex-1">
+				<button className="  p-2 m-2 border-solid border-2  border-blue-800 rounded-md" onClick={addTask}>
 					Add Task
 				</button>
 			</div>
