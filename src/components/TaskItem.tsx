bb66e9ffec16cdb092ce28dc1297bb46c60e40ee
Piotr Wojenski <pwojenskip@gmail.com>
2024-01-14 20:59:00 +0100

    Add EditIcon to TaskItem component

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
+import EditIcon from '@mui/icons-material/Edit'
 
 const TaskItem = (props: any) => {
 	return (
@@ -7,7 +8,7 @@ const TaskItem = (props: any) => {
 			<h2 className={props.taskInfo.isCompleted ? 'line-through' : ''}>{props.taskInfo.value}</h2>
 			<DeleteForeverIcon onClick={() => props.removeTask(props.taskInfo.id)} className="cursor-pointer" />
 			<input onClick={() => props.completeTask(props.taskInfo.id)} type="checkbox" />
-			<button onClick={() => props.changeEditMode(props.taskInfo.id)}> Edit</button>
+			<EditIcon onClick={() => props.changeEditMode(props.taskInfo.id)} className="cursor-pointer" />
 		</li>
 	)
 }
