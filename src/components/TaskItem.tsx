8295990d65852e6218f3a3546f39450da085a546
Piotr Wojenski <pwojenskip@gmail.com>
2024-01-14 20:52:45 +0100

    Refactor CSS and add cursor pointer to delete icon

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -5,7 +5,7 @@ const TaskItem = (props: any) => {
 	return (
 		<li className="border border-solid border-black rounded-md p-2 m-2 overflow-hidden bg-gray-200">
 			<h2 className={props.taskInfo.isCompleted ? 'line-through' : ''}>{props.taskInfo.value}</h2>
-			<DeleteForeverIcon onClick={() => props.removeTask(props.taskInfo.id)} />
+			<DeleteForeverIcon onClick={() => props.removeTask(props.taskInfo.id)} className="cursor-pointer" />
 			<input onClick={() => props.completeTask(props.taskInfo.id)} type="checkbox" />
 			<button onClick={() => props.changeEditMode(props.taskInfo.id)}> Edit</button>
 		</li>
