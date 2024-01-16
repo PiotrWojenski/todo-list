3dce5406ca926425d9eac900d6c5d853fdb5e506
Piotr Wojenski <pwojenskip@gmail.com>
2024-01-16 15:53:47 +0100

    Add styling and functionality to TaskItem component

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -5,23 +5,30 @@ import Tooltip from '@mui/material/Tooltip'
 
 const TaskItem = (props: any) => {
 	return (
-		<li className="border border-solid border-black rounded-md p-2 m-2 overflow-hidden bg-gray-200">
+		<li
+			className={
+				props.taskInfo.isCompleted
+					? 'border border-solid border-black rounded-md p-2 m-2 overflow-hidden bg-green-400 flex justify-between items-center'
+					: 'border border-solid border-black rounded-md p-2 m-2 overflow-hidden bg-gray-200 flex justify-between items-center'
+			}>
 			<h2 className={props.taskInfo.isCompleted ? 'line-through' : ''}>{props.taskInfo.value}</h2>
-			<Tooltip title="Delete Task" arrow>
-				<DeleteForeverIcon
-					onClick={() => props.removeTask(props.taskInfo.id)}
-					className="cursor-pointer hover:text-red-500"
-				/>
-			</Tooltip>
-			<Tooltip title="Complete Task" arrow>
-				<input onClick={() => props.completeTask(props.taskInfo.id)} type="checkbox" title="completed" />
-			</Tooltip>
-			<Tooltip title="Edit Task" arrow>
-				<EditIcon
-					onClick={() => props.changeEditMode(props.taskInfo.id)}
-					className="cursor-pointer hover:text-green-600"
-				/>
-			</Tooltip>
+			<div className="flex space-x-2">
+				<Tooltip title="Delete Task" arrow>
+					<DeleteForeverIcon
+						onClick={() => props.removeTask(props.taskInfo.id)}
+						className="cursor-pointer hover:text-red-500"
+					/>
+				</Tooltip>
+				<Tooltip title="Complete Task" arrow>
+					<input onClick={() => props.completeTask(props.taskInfo.id)} type="checkbox" title="completed" />
+				</Tooltip>
+				<Tooltip title="Edit Task" arrow>
+					<EditIcon
+						onClick={() => props.changeEditMode(props.taskInfo.id)}
+						className="cursor-pointer hover:text-green-600"
+					/>
+				</Tooltip>
+			</div>
 		</li>
 	)
 }
